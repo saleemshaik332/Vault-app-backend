48bc3d3013ff4f377b862fca2b73b9cac0a81e9c
saleemshaik332 <sheikhselim4455@gmail.com>
2026-09-21 17:19:06 +0530

    Allow GitHub Pages frontend in CORS

--- vaultproject/settings.py
+++ vaultproject/settings.py
@@ -65,7 +65,7 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = os.environ.get(
     'CORS_ALLOWED_ORIGINS',
-    'http://localhost:8080,http://127.0.0.1:8080,http://localhost:5500,http://127.0.0.1:5500'
+    'http://localhost:8080,http://127.0.0.1:8080,http://localhost:5500,http://127.0.0.1:5500,https://saleemshaik332.github.io'
 ).split(',')
 
 REST_FRAMEWORK = {
